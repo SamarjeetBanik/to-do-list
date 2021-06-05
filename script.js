842a0a727c8d9f6cab477f6ab80a2f5f594344e9
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-06-05 10:21:15 +0530

    updated conditional functionality on enter/btn click

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-var input = document.getElementById("writespace");
+var input = document.getElementById("writespace")
 
-input.addEventListener("keyup", function(event) {
-  if(event.keyCode === 13) {
+input.addEventListener("keyup", (event) => {
+  if(event.keyCode === 13 && input.value !== "") {
     // event.preventDefault();
     // document.getElementById("btn").click();
 
     AddTask(input.value)
     document.getElementById("writespace").value = "";
     /* console.log("Enter key pressed"); */
+  } else if(event.keyCode === 13 && input.value === "") {
+    alert("Please enter something to proceed")
   }
 })
 
 console.log("reached")
 
-document.getElementById("btn").addEventListener("click", function(event) {
-    AddTask(input.value)
-    document.getElementById("writespace").value = "";
-    /* console.log("Enter key pressed"); */
+document.getElementById("btn").addEventListener("click", (event) => {
+    if(input.value === "") {
+      alert("Please enter something to proceed")
+    } else {
+      AddTask(input.value)
+      document.getElementById("writespace").value = "";
+      /* console.log("Enter key pressed"); */
+    }
 })
